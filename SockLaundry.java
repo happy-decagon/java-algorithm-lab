c5fe53613812d3722dcf0c57373bc32ff7b5ba2c
Happy <68383459+happy-decagon@users.noreply.github.com>
2022-01-13 13:47:51 +0100

    Create SockLaundry.java

---
+++ SockLaundry.java
@@ -0,0 +1,14 @@
+package socklaundering;
+
+public class SockLaundry {
+
+    //Do not delete or edit this method, you can only modify the block
+    public int getMaximumPairOfSocks(int noOfWashes, int[] cleanPile, int[] dirtyPile) {
+        //You can edit from here down
+        return 0;
+    }
+
+    /**
+     * You can create various helper methods
+     * */
+}
